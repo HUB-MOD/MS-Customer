418de79ef368ead7fe45bd74161911ddfce6c495
Heber Sixto Uraccahua Barrios <huraccahuab@gmail.com>
2022-03-09 21:31:01 -0500

    fix configuration

--- MSCustomer/src/main/java/com/nttdata/BootCampProyectoIG6/BootCampProyectoIg6Application.java
+++ MSCustomer/src/main/java/com/nttdata/BootCampProyectoIG6/BootCampProyectoIg6Application.java
@@ -6,17 +6,19 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-
-
+@EnableEurekaClient
 @SpringBootApplication
 public class BootCampProyectoIg6Application implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootCampProyectoIg6Application.class, args);
 	}
+
 	private static final Logger logger = LogManager.getLogger(BootCampProyectoIg6Application.class);
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		logger.debug("Debug de la aplicación");
